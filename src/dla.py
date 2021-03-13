2cb2849339868dfeca14b43678f57861a392e1eb
Alex Tyshka <atyshka15@gmail.com>
2021-03-12 23:20:27 -0500

    Add half precision

--- src/dla.py
+++ src/dla.py
@@ -4,20 +4,22 @@ from tensorflow.keras import layers
 
 
 class ResnetBlock(layers.Layer):
-  def __init__(self, num_filters, kernel_size=(3, 3), strides=(1, 1), reshape=False, name=None):
-    super(ResnetBlock, self).__init__(name=name)
+  def __init__(self, num_filters, kernel_size=(3, 3), strides=(1, 1), reshape=False, name=None, dtype=None):
+    super(ResnetBlock, self).__init__(name=name, dtype=dtype)
 
-    self.conv1 = layers.Conv2D(num_filters, kernel_size, strides=strides, use_bias=False, padding='same')
-    self.bn1 = layers.BatchNormalization()
+    self.conv1 = layers.Conv2D(num_filters, kernel_size, strides=strides, use_bias=False, padding='same', dtype=dtype)
+    self.bn1 = layers.BatchNormalization(dtype=dtype)
 
-    self.conv2 = layers.Conv2D(num_filters, kernel_size, use_bias=False, padding='same')
-    self.bn2 = layers.BatchNormalization()
+    self.conv2 = layers.Conv2D(num_filters, kernel_size, use_bias=False, padding='same', dtype=dtype)
+    self.bn2 = layers.BatchNormalization(dtype=dtype)
 
     if reshape or strides != (1, 1):
         # Need to match skip connection dimensions to new dimensions
-        self.skip_conn = keras.Sequential()
-        self.skip_conn.add(layers.Conv2D(filters=num_filters, kernel_size=(1, 1), strides=strides, use_bias=False))
-        self.skip_conn.add(layers.BatchNormalization())
+        self.conv3 = layers.Conv2D(filters=num_filters, kernel_size=(1, 1), strides=strides, use_bias=False, dtype=dtype)
+        self.bn3 = layers.BatchNormalization(dtype=dtype)
+        self.skip_conn = lambda x, **kwargs: self.bn3(self.conv3(x))
+        # self.skip_conn.add()
+        # self.skip_conn.add()
     else:
         # Do nothing
         self.skip_conn = lambda x, **kwargs: x
@@ -35,28 +37,28 @@ class ResnetBlock(layers.Layer):
     return tf.nn.relu(x)
 
 class FeatureExtractor(layers.Layer):
-    def __init__(self, num_filters, num_blocks=6, downsample=True, reshape=False):
-        super(FeatureExtractor, self).__init__(name='FeatureExtractor')
+    def __init__(self, num_filters, num_blocks=6, downsample=True, reshape=False, dtype=None):
+        super(FeatureExtractor, self).__init__(name='FeatureExtractor', dtype=dtype)
 
         self.sequence = keras.Sequential()
         # Downsample by 2 along horizontal
         if downsample:
-            self.sequence.add(ResnetBlock(num_filters, strides=(1, 2), name="Downsample", reshape=reshape))
+            self.sequence.add(ResnetBlock(num_filters, strides=(1, 2), name="Downsample", reshape=reshape, dtype=dtype))
         for i in range(num_blocks):
-            self.sequence.add(ResnetBlock(num_filters, name="Resnet_%i" % i, reshape=reshape))
+            self.sequence.add(ResnetBlock(num_filters, name="Resnet_%i" % i, reshape=reshape, dtype=dtype))
 
     def call(self, input_tensor, training=False):
         return self.sequence(input_tensor, training=training)
 
 class FeatureAggregator(layers.Layer):
-    def __init__(self, num_filters):
-        super(FeatureAggregator, self).__init__(name='FeatureAggregator')
+    def __init__(self, num_filters, dtype=None):
+        super(FeatureAggregator, self).__init__(name='FeatureAggregator', dtype=dtype)
 
-        self.upsample = layers.Conv2DTranspose(num_filters, (3, 3), (1,2), padding='same')
-        self.bn = layers.BatchNormalization()
-        self.concat = layers.Concatenate()
-        self.block1 = ResnetBlock(num_filters, reshape=True)
-        self.block2 = ResnetBlock(num_filters)
+        self.upsample = layers.Conv2DTranspose(num_filters, (3, 3), (1,2), padding='same', dtype=dtype)
+        self.bn = layers.BatchNormalization(dtype=dtype)
+        self.concat = layers.Concatenate(dtype=dtype)
+        self.block1 = ResnetBlock(num_filters, reshape=True, dtype=dtype)
+        self.block2 = ResnetBlock(num_filters, dtype=dtype)
 
     def call(self, fine_input, coarse_input, training=False):
         x = self.upsample(coarse_input)
--- src/model.py
+++ src/model.py
@@ -6,16 +6,17 @@ from dla import FeatureAggregator, FeatureExtractor
 class LaserNet(keras.Model):
     def __init__(self, **kwargs):
         super(LaserNet, self).__init__(**kwargs)
-        self.block_1a = FeatureExtractor(64, downsample=False, reshape=True)
-        self.block_1b = FeatureAggregator(64)
-        self.block_2a = FeatureExtractor(64)
-        self.block_1c = FeatureAggregator(64)
-        self.block_2b = FeatureAggregator(64)
-        self.block_3a = FeatureExtractor(64)
+        policy = tf.keras.mixed_precision.Policy('mixed_float16')
+        self.block_1a = FeatureExtractor(64, downsample=False, reshape=True, dtype=policy)
+        self.block_1b = FeatureAggregator(64, dtype=policy)
+        self.block_2a = FeatureExtractor(64, dtype=policy)
+        self.block_1c = FeatureAggregator(64, dtype=policy)
+        self.block_2b = FeatureAggregator(64, dtype=policy)
+        self.block_3a = FeatureExtractor(64, dtype=policy)
         self.predict = PredictionTransform(1, [1])
         self.corners = BoxToCorners()
 
-    @tf.function(experimental_compile=True)
+    @tf.function
     def call(self, inputs, training=False):
         (inputs, xy) = inputs
         extract_1 = self.block_1a(inputs, training=training)
@@ -23,7 +24,7 @@ class LaserNet(keras.Model):
         extract_3 = self.block_3a(extract_2, training=training)
         aggregate_1 = self.block_1b(extract_1, extract_2, training=training)
         aggregate_2 = self.block_2b(extract_2, extract_3, training=training)
-        raw_output = self.block_1c(aggregate_1, aggregate_2, training=training)
+        raw_output = tf.cast(self.block_1c(aggregate_1, aggregate_2, training=training), tf.float32)
         classes, centers, alphas, log_stddevs = self.predict(raw_output, training=training)
         azimuth = inputs[:, :, :, 2]
         corners = self.corners(centers, xy, azimuth)
